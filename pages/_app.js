dc8852252d52dc5c05a94f324991c137085b6777
tare <59001439+junghwa1996@users.noreply.github.com>
2024-11-29 18:14:12 +0900

    ✨ Next 13 버전에서 구글 폰트 사용하기

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,12 @@ import Header from '@/components/Header';
 import { ThemeProvider } from '@/lib/ThemeContext';
 import '@/styles/globals.css';
 import Head from 'next/head';
+import { Noto_Sans_KR } from '@next/font/google';
+
+const notoSansKR = Noto_Sans_KR({
+  weight: ['400', '700'],
+  subsets: [],
+});
 
 export default function App({ Component, pageProps }) {
   return (
