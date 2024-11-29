2b9a7863218f144e40d3ed17571b68cf561aded9
tare <59001439+junghwa1996@users.noreply.github.com>
2024-11-29 20:54:38 +0900

    ✨ 실습을 위한 Spinner 컴포넌트 추가

--- next.config.js
+++ next.config.js
@@ -4,9 +4,9 @@ const nextConfig = {
   async redirects() {
     return [
       {
-        source: '/movies/:id', // 리다이렉션의 시작 URL
-        destination: '/films/:id', // 리다이렉션의 대상 URL
-        permanent: true, // 영구(301) 리다이렉트
+        source: '/movies/:id',
+        destination: '/films/:id',
+        permanent: true,
       },
     ];
   },
